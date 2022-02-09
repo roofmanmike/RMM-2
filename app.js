2788c9aa4d177be3b8100d3ea96dc970a5270f09
Mike Foley <roofmanmike@gmail.com>
2022-02-09 16:42:47 -0500

    red label, bootstrap container fluid

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const app = express();
+const sunny = false;
 app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
   postFunc();
 });
+
 // postFunc();
 function postFunc(){
 app.post("/", function(req, res){
