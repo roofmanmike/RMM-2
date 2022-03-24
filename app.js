821eabf4308b3289138c909799f77604203c4c98
Mike Foley <roofmanmike@gmail.com>
2022-03-23 20:42:14 -0400

    version3-23

--- app.js
+++ app.js
@@ -1,50 +1,83 @@
-const express = require("express");
-const https = require("https");
-const bodyParser = require("body-parser");
-const app = express();
-const sunny = false;
-// import { Config } from 'config.js';
-// add comment for push
+//jshint esversion6
 
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
-  postFunc();
 });
-
-// postFunc();
-function postFunc(){
 app.post("/", function(req, res){
-    // const apiKey = Config.MY_KEY;
-    const apiKey = "379bf63134d46434dd821bc2b954bd92#";
-    const query = req.body.cityName;
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=imperial&appid=" + apiKey;
-    https.get(url, function(response){
+  let today = new Date();
+let dd = today.getDate();
 
-      console.log(response.statusCode);
-      response.on("data", function(data){
-        const weatherData = JSON.parse(data);
-        const temp = weatherData.main.temp;
-        const icon = weatherData.weather[0].icon;
-        const weatherDescription = weatherData.weather[0].description;
-        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-        res.write("<head><meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />");
-        res.write("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>");
-        res.write("<meta charset='utf-8'>");
-        res.write("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>");
-        res.write("</head>")
-        res.write("<body style= 'background-image:url(https://scontent.fdet1-1.fna.fbcdn.net/v/t1.6435-9/67644064_107197060639175_7781083183763685376_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=KJWmbJNLkekAX-9r7V0&_nc_ht=scontent.fdet1-1.fna&oh=00_AT_uYdcASVyhzyDs_d90vXcYmjTfjbsSZArCrEgXXSvWlw&oe=623492BB)'></body>");
-        res.write("<h1>Right now:  " + weatherDescription + "</h1>");
-        res.write("<h1>The temperature in " + query + " is " + temp + " degrees</h1>");
-        res.write("<img src=" + imageURL + ">");
-        res.write("<br>");
-        res.write("<h1><a href='https://www.roofmanmike.com'>RoofManMike</a><h1></body>");
-        res.send();
-      });
-    });
-})
+let mm = today.getMonth()+1; 
+const yyyy = today.getFullYear();
+if(dd<10) 
+{
+    dd=`0${dd}`;
+} 
+
+if(mm<10) 
+{
+    mm=`0${mm}`;
+} 
+today = `${mm}-${dd}-${yyyy}`;
+
+
+
+var num1 = Number(req.body.num1);
+var num2 = Number(req.body.num2);
+var num3 = Number(req.body.num3);
+var num4 = Number(req.body.num4);
+var num5 = Number(req.body.num5);
+
+var height = num5;
+var area = num1 * num2 * 5.5;
+var squares = num1 * num2;
+var gutter = num3 * 15;
+var coping = num4 * 12;
+
+
+
+if (num5 > 2){
+  height = height * 1000;
+}
+else{height=0};
+
+var result = area + num3 + gutter + coping + height;
+let ob = new Intl.NumberFormat('en-US');
+squares = ob.format(squares);
+area = ob.format(area);
+gutter = ob.format(gutter);
+coping = ob.format(coping);
+result = ob.format(result)
+res.write('<head><style>body{background-color:darkgrey; text-align:center;}hr{background-color:red;}</style></head>');
+res.write('<body>');
+res.write('<h1>Valid thru 30 days from: ' + today + ': </h1>');
+res.write('<h1>' + squares + ' sq ft</h1>');
+
+
+res.write('<h1>Before termination(s) and watershed: <span style="color:green">$</span>' + area + '</h1><hr>');
+if (gutter > 0) {
+  res.write('<h1>Gutters: <span style="color:green">$</span>' + gutter + '</h1>');  
+}
+if (coping >= 1) {
+  res.write('<h1>Coping: <span style="color:green">$</span>' + coping + '</h1>');  
+}
+if (height > 0) {
+  res.write('<h1>' + num5 + ' story hazard: <span style="color:green">$</span>' + height + '</h1>');
+  
 }
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Server is running on port 3000");
+res.write('<h1 style="text-decoration:underline">Total: <span style="color:green">$</span>' + '<span style="color:red">' + result + '</span></h1>');
+res.write('</body>' + '<footer>');
+res.write('<img src="https://scontent.fdet1-1.fna.fbcdn.net/v/t1.6435-9/122760339_422026922489519_6417623735746863165_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=uTlQ-O9OijcAX98A3Fh&_nc_oc=AQkAH02PoTwcDSYYYP9lFtcfgEjxaJsyphVuQuUcUj6hvXxQyk80JdTxO4mjPbo8Vt4&_nc_ht=scontent.fdet1-1.fna&oh=00_AT_wD6qOn7rgEIDYC1gU67Ce35FZLmQKpms_Cx0Vq6aXEA&oe=6249EEFB" alt="">');
+  res.send();
 })
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000.");
+});
